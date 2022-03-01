b279d6139a1a482f8283cb2a883d773c4822117f
Alejandro Chavez <achavezr5@wisc.edu>
2022-02-28 21:40:59 -0600

    Initial commit

---
+++ Shared/An_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  An_AppApp.swift
+//  Shared
+//
+//  Created by Alejandro Chavez on 2/28/22.
+//
+
+import SwiftUI
+
+@main
+struct An_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
